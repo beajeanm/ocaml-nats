9943e6905994a6db8878aa8f2fa60bdbb14c9318
Jean-Michel Bea <code@jm-bea.net>
2020-06-21 22:27:18 +0100

    Simplify the implementation by using parse_many

--- lib/client.ml
+++ lib/client.ml
@@ -2,10 +2,6 @@ open Lwt.Infix
 
 type t = {output: Lwt_io.output_channel}
 
-type message_processor = (Messages.t, string) result -> unit Lwt.t
-
-type reader_state = {in_chan: Lwt_io.input_channel; leftover: Bigstringaf.t}
-
 let create_connection host port =
   let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
   let host = Unix.gethostbyname host in
@@ -16,37 +12,13 @@ let create_connection host port =
   let oc = Lwt_io.of_fd ~mode:Output socket in
   (ic, oc)
 
-let read_next_message client =
-  let state = Angstrom.Buffered.parse Protocol.parser in
-  let updated_state =
-    Angstrom.Buffered.feed state (`Bigstring client.leftover)
-  in
-  Angstrom_lwt_unix.with_buffered_parse_state updated_state client.in_chan
-  >|= fun (unconsumed, result) ->
-  let leftover =
-    Bigstringaf.sub unconsumed.buf ~off:unconsumed.off ~len:unconsumed.len
-  in
-  ({client with leftover}, result)
-
-let create_stream client =
-  let state = ref client in
-  let f () =
-    read_next_message !state
-    >|= fun (new_client, result) ->
-    state := new_client ;
-    Some result
-  in
-  Lwt_stream.from f
-
 let process_message client push = function
-  | Ok Messages.Ping ->
-      Lwt.async (fun () -> Lwt_io.write client.output "Pong\r\n")
-  | Ok Messages.Ok | Ok (Messages.Info _) ->
-      ()
-  | Ok (Messages.Err msg) ->
-      Lwt.async (fun () -> push (Result.Error msg))
+  | Messages.Ping ->
+      Lwt_io.write client.output "Pong\r\n"
+  | Messages.Ok | Messages.Info _ ->
+      Lwt.return_unit
   | _ as msg ->
-      Lwt.async (fun () -> push msg)
+      push msg
 
 let pub ~msg ~subject client =
   let formatted_msg =
@@ -59,15 +31,12 @@ let sub ~subject ?(queue = "") ~sid client =
   Lwt_io.write client.output formatted_msg
 
 let start ~host ~port push =
-  let rec read_loop stream client : unit Lwt.t =
-    Lwt_stream.next stream
-    >>= fun msg ->
-    process_message client push msg ;
-    read_loop stream client
-  in
   create_connection host port
   >|= fun (ic, oc) ->
   let client = {output= oc} in
-  let reader_state = {in_chan= ic; leftover= Bigstringaf.create 0} in
-  Lwt.async (fun () -> read_loop (create_stream reader_state) client) ;
-  client
+  let filtered_push = process_message client push in
+  let (promise, resolver) = Lwt.wait () in
+  Lwt.async (fun () ->
+      Angstrom_lwt_unix.parse_many Protocol.parser filtered_push ic
+      >|= fun _ -> Lwt.wakeup resolver ()) ;
+  (client, promise)
--- lib/client.mli
+++ lib/client.mli
@@ -1,9 +1,7 @@
 type t
 
-type message_processor = (Messages.t, string) result -> unit Lwt.t
-
 val pub : msg:string -> subject:string -> t -> unit Lwt.t
 
 val sub : subject:string -> ?queue:string -> sid:string -> t -> unit Lwt.t
 
-val start : host:string -> port:int -> message_processor -> t Lwt.t
+val start : host:string -> port:int -> (Messages.t -> unit Lwt.t) -> (t * unit Lwt.t) Lwt.t
